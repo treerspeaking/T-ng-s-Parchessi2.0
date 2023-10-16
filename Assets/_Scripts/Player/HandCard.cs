e2e8fcc59222b9600d412f045f79e292bf4208e7
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-10-16 17:21:36 +0700

    feat: hand card prefab

--- Assets/_Scripts/Player/HandCard.cs
+++ Assets/_Scripts/Player/HandCard.cs
@@ -8,10 +8,15 @@ using UnityEngine.UI;
 public class HandCard : MonoBehaviour
 {
     public CardDescription CardDescription;
+    
     [SerializeField] private TMP_Text _cardName;
     [SerializeField] private TMP_Text _cardDescription;
     [SerializeField] private Image _cardImage;
     [SerializeField] private TMP_Text _cardCost;
+    
+    [SerializeField] private Image _backgroundImage;
+    [SerializeField] private Image _descriptionBackgroundImage;
+    
     
     private void Start()
     {
