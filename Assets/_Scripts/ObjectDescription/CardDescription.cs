6cf8baac8cc8ce03622e3789c350bb6182009bdd
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-10-16 08:24:33 +0700

    feat: card, dice, pawn description

---
+++ Assets/_Scripts/ObjectDescription/CardDescription.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace _Scripts.Scriptable_Objects
+{
+    [CreateAssetMenu(fileName = "CardDescription", menuName = "ScriptableObjects/CardDescription", order = 1)]
+    public class CardDescription : ScriptableObject
+    {
+        public int CardID;
+        public Sprite CardSprite;
+        public string CardName;
+        public string CardDescriptionText;
+        public int CardCost;
+        
+    }
+}
---
+++ Assets/_Scripts/ObjectDescription/DiceDescription.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "DiceDescription", menuName = "ScriptableObjects/DiceDescription", order = 1)]
+public class DiceDescription : ScriptableObject
+{
+    public int DiceID;
+}
