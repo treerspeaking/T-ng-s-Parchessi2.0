5710864f550d1f4222079eeba787c2c979379f3c
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-10-20 20:46:48 +0700

    fix: change network List to array

--- Assets/_Scripts/Player/PlayerControllerScript/PlayerResourceController.cs
+++ Assets/_Scripts/Player/PlayerControllerScript/PlayerResourceController.cs
@@ -9,13 +9,17 @@ using Random = UnityEngine.Random;
 
 public class PlayerResourceController : NetworkBehaviour
 {
+    const int DICE_HAND_SIZE = 3;
+    const int CARD_HAND_SIZE = 5;
+    private static readonly CardContainer EmptyCardContainer = new CardContainer{CardID = -1};
+    private static readonly DiceContainer EmptyDiceContainer = new DiceContainer{DiceID = -1};
 
     public NetworkList<CardContainer> DeckCards;
-    public NetworkList<CardContainer> HandCards;
-    public NetworkList<CardContainer> DiscardCards;
+    public NetworkList<CardContainer> HandCards; // This Must work as array
+    public NetworkList<CardContainer> DiscardCards; 
     
     public NetworkList<DiceContainer> IncomeDices;
-    public NetworkList<DiceContainer> CurrentTurnDices;
+    public NetworkList<DiceContainer> CurrentTurnDices; // This Must work as array
 
     private PlayerDiceHand _playerDiceHand;
     private PlayerCardHand _playerCardHand;
@@ -23,11 +27,25 @@ public class PlayerResourceController : NetworkBehaviour
     private void Awake()
     {
         DeckCards = new();        
-        HandCards = new();        
+        HandCards = new(new CardContainer[CARD_HAND_SIZE]);        
         DiscardCards = new();     
                           
         IncomeDices = new();      
-        CurrentTurnDices = new(); 
+        CurrentTurnDices = new(new DiceContainer[DICE_HAND_SIZE]);
+
+    }
+
+    private void Start()
+    {
+        for (var index = 0; index < HandCards.Count; index++)
+        {
+            HandCards[index] = EmptyCardContainer;
+        }
+        
+        for (var index = 0; index < CurrentTurnDices.Count; index++)
+        {
+            CurrentTurnDices[index] = EmptyDiceContainer;
+        }
     }
 
     public override void OnNetworkSpawn()
@@ -57,6 +75,7 @@ public class PlayerResourceController : NetworkBehaviour
     public void GainIncomeServerRPC()
     {
         DiceContainer[] addDiceContainers = new DiceContainer[IncomeDices.Count]; // RPC came before NetworkList 
+        CurrentTurnDices.Clear();
         for (var index = 0; index < IncomeDices.Count; index++)
         {
             var diceContainer = IncomeDices[index];
@@ -90,8 +109,20 @@ public class PlayerResourceController : NetworkBehaviour
         int index = Random.Range(0, DeckCards.Count);
         var card = DeckCards[index];
         DeckCards.RemoveAt(index);
-        HandCards.Add(card);
-        AddCardToHandClientRPC(card, HandCards.Count - 1);
+
+        int handCardContainerIndex = -1;
+        for (var i = 0; i < HandCards.Count; i++)
+        {
+            var cardContainer = HandCards[i];
+            if (cardContainer.Equals(EmptyCardContainer))
+            {
+                handCardContainerIndex = i;
+                HandCards[i] = card;
+                break;
+            }
+        }
+
+        AddCardToHandClientRPC(card, handCardContainerIndex);
     }
     
     [ClientRpc]
@@ -103,14 +134,18 @@ public class PlayerResourceController : NetworkBehaviour
     [ServerRpc]
     public void RemoveDiceServerRPC(int index)
     {
-        CurrentTurnDices.RemoveAt(index);
+        //CurrentTurnDices.RemoveAt(index);
+        CurrentTurnDices[index] = EmptyDiceContainer;
     }
     
     [ServerRpc]
     public void RemoveCardFromHandServerRPC(int handCardContainerIndex)
     {
         DiscardCards.Add(HandCards[handCardContainerIndex]);
-        HandCards.RemoveAt(handCardContainerIndex);
+        //HandCards.RemoveAt(handCardContainerIndex);
+        HandCards[handCardContainerIndex] = EmptyCardContainer;
     }
 
+    
+    
 }
