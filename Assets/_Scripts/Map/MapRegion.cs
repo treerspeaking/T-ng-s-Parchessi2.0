8f92768aff5063d6cb08d5d45140ac28d04863d5
Minh Duy <duy.nguyen233@hcmut.edu.vn>
2023-10-25 19:20:02 +0700

    add path

---
+++ Assets/_Scripts/Map/MapRegion.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using _Scripts.Map;
+
+public class MapRegion : MonoBehaviour
+{
+    [SerializeField] private Transform _mapParent;
+    [SerializeField] private List<MapPath> _mapPaths = new ();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
