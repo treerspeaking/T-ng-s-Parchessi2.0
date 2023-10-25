85b255b0259f49435aeb82a7c5847ed99eeab168
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-10-25 20:45:55 +0700

    feat: map Manager to map region

--- Assets/_Scripts/Game/Map/MapRegion.cs
+++ Assets/_Scripts/Game/Map/MapRegion.cs
@@ -8,14 +8,17 @@ public class MapRegion : MonoBehaviour
     [SerializeField] private Transform _mapParent;
     [SerializeField] private List<MapPath> _mapPaths = new ();
     // Start is called before the first frame update
-    void Start()
+    
+    public MapPath GetMapPath(int pathIndex)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (pathIndex < _mapPaths.Count)
+        {
+            return _mapPaths[pathIndex];
+        }
+        else
+        {
+            Debug.LogError("MapPath index out of range");
+        }
+        return null;
     }
 }
